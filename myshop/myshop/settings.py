33de0f654b439676b9f945794453bb38e8844cfa
Ruzal Zakirov <email.com>
2023-05-17 13:56:51 +0300

    Myshop v.1
    Getting started creating an app

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$6ma-ru9q+e&px9560@3sop^g2_dm=mp^jl1pogtvb58o2k(t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfg',
 ]
 
 MIDDLEWARE = [
