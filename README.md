f8b6ec5eb81f661fad7e5dfc11d5628202fa0973
Ruzal <email.com>
2023-08-22 16:38:02 +0300

    Update settings.py and README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# myshop
-MyShop is a cutting-edge online retail platform that offers a wide range of high-quality products to meet your everyday needs
+# WORKPLACE
+### Описание
+WORKPLACE — передовая онлайн-платформа для розничной торговли, предлагающая широкий ассортимент высококачественных товаров для удовлетворения ваших повседневных потребностей.
+
+### Используемые технологии:
+- Python 3.11
+- Django 4.1
+- Pillow 9.2
+### Как развернуть проект локально:
+Клонируйте репозиторий:
+```
+     git clone https://github.com/Ruzal-Z/myshop.git
+```
+или используйте SSH-ключ:
+```
+     git clone git@github.com:Ruzal-Z/myshop.git
+```
+Установить и активировать виртуальную среду
+```
+     python -m venv venv
+```
+```
+     source venv/Scripts/activate
+```
+Установить зависимости из файла requirements.txt
+```
+     pip install -r requirements.txt
+```
+Сделать миграцию:
+```
+     python manage.py migrate
+```
+Запустите проект в режиме разработки:
+```
+     cd myshop/  
+```
+```
+     python manage.py runserver
+```
+Откройте в своем браузере `localhost` или `127.0.0.1:8000`
+
+## Автор:
+Рузал Закиров [GitHub](https://github.com/Ruzal-Z/)
--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -1,19 +1,22 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfbgerleruh65464'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost,http://127.0.0.1',
+).split(',')
 
 
 # Application definition
@@ -64,28 +67,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myshop.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myshopdb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',  # '127.0.0.1',
-        'PORT': '5432',
+if os.getenv('USE_SQLITE', 'false').lower() in ('true', '1'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('POSTGRES_HOST', ''),
+            'PORT': os.getenv('POSTGRES_PORT', 5432),
+        }
+    }
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,25 +103,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+MEDIA_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
